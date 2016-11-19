52077ca5c8de0e0daf0296cddddcc1a8f83088ff
Prasad Khegade <prasad.schoolerp@gmail.com>
2016-11-19 16:06:53 +0530

    Plans page - Added controller logic to fetch slider images - css for plans page - sms img

--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -2,38 +2,39 @@
  * Copyright (c) 2014 VMware, Inc. All rights reserved.
  ******************************************************************************/
 (function() {
-	angular
-	.module('koStation')
-	.controller('MainController', ['$scope', '$rootScope', '$timeout', '$mdSidenav', '$log', '$http',
-        function($scope, $rootScope, $timeout, $mdSidenav, $log, $http) {
+    angular
+        .module('koStation')
+        .controller('MainController', ['$scope', '$rootScope', '$timeout', '$mdSidenav', '$log', '$http',
+            function($scope, $rootScope, $timeout, $mdSidenav, $log, $http) {
 
-            console.log("Inside MainController....");
+                console.log("Inside MainController....");
 
-            $scope.toggleLeft = buildToggler('left');
+                $scope.toggleLeft = buildToggler('left');
 
-            $scope.toggleRight = buildToggler('right');
-            function buildToggler(componentId) {
-              return function() {
-                $mdSidenav(componentId).toggle();
-              }
+                $scope.toggleRight = buildToggler('right');
+
+                function buildToggler(componentId) {
+                    return function() {
+                        $mdSidenav(componentId).toggle();
+                    }
+                }
+
+                $scope.fetchCarouselSlider = function() {
+                    var api = 'php/api/v1/getCarousalData';
+                    $http({
+                        method: 'GET',
+                        url: api
+                    }).then(function(response) {
+                        if(response.data) {
+
+                        }
+                    }, function(response) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                    });
+                };
+
+                $scope.fetchCarouselSlider();
             }
-
-           /* $scope.sendMail = function() {
-                console.log("$$"+JSON.stringify($scope.user));
-                var api = './php/mail/mail.php?' + 'name=' + $scope.user.name + '&contact=' + $scope.user.contactno + '&email=' + $scope.user.email + '&msg=' + $scope.user.message;
-                $http({
-                  method: 'GET',
-                  url: api
-                }).then(function(response) {
-                    // this callback will be called asynchronously
-                    // when the response is available
-                  }, function(response) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                  });
-            };*/
-    
-            
-        }
-    ]);
+        ]);
 })();
