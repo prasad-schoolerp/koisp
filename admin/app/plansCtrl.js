d00c6a2c540b0935a5dffb4eef40bc5b845302de
Ketan-PC\Ketan <shikhare.ketan@gmail.com>
2016-11-25 00:11:28 +0530

    Added plans page pop up table css fix added plans table

--- admin/app/plansCtrl.js
+++ admin/app/plansCtrl.js
@@ -1,10 +1,28 @@
 app.controller('plansCtrl', function ($scope, $rootScope, $routeParams, $location, $http, Data,ShareData) {
 
 $scope.$on('$viewContentLoaded', function(event) {
- 		
+ 		$(function() {
+    //----- OPEN
+    $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+ 
+        e.preventDefault();
+    });
+ 
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+ 
+        e.preventDefault();
+    });
+});
 });
 
-
+$scope.showCategorypopUp = function(){
+	$("#popup").show();
+}
 	    $rootScope.logout = function () {
         Data.get('logout').then(function (results) {
             Data.toast(results);
